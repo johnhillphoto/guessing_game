ac0ffbabdbe85e6ff865de0b8d009ab291f1efb9
John Hill <john@proofpositiveimaging.com>
2015-12-23 15:35:26 -0500

    Initial functions running, no hint or play again yet

---
+++ scripts/guessingGame.js
@@ -0,0 +1,149 @@
+/* **** Global Variables **** */
+// try to elminate these global variables in your project, these are here just to start.
+
+var winningNumber,tries=[];
+
+
+
+/* **** Guessing Game Functions **** */
+
+// Generate the Winning Number
+
+function generateWinningNumber(){
+  var winningNum=Math.random();
+  winningNum*=100;
+  winningNum=Math.floor(winningNum);
+  return winningNum;
+}
+
+// Fetch the Players Guess
+
+function playersGuessSubmission(){
+  var playersGuess = $("#numberinput").val();
+  playersGuess = Number(playersGuess);
+  checkGuess(playersGuess);
+  $("#numberinput").val("");
+}
+
+// Determine if the next guess should be a lower or higher number
+
+function lowerOrHigher(playersGuess){
+  if (playersGuess > winningNumber){
+      return "Your guess is higher";
+  }
+  else {
+      return "Your guess is lower";
+  }
+}  //end lowerOrHigher
+
+function guessMessage(playersGuess){
+      var string="";
+      var lowOrHigh = lowerOrHigher(playersGuess);
+      var winDistance = playersGuess-winningNumber;
+      winDistance=Math.abs(winDistance);
+      if(winDistance>20){
+        string=lowOrHigh + " and more than 20 digits away.";
+      }
+      else if((winDistance <= 20) && (winDistance > 10)){
+        string=lowOrHigh;
+      }
+      else if(winDistance <= 5){
+        string=lowOrHigh + " and within 5 digits away.";
+      }
+      else if(winDistance <= 10){
+        string=lowOrHigh + " and within 10 digits away.";
+      }
+      return string;    
+} //end guessMessage
+
+// Check if the Player's Guess is the winning number 
+
+function checkGuess(playersGuess){
+  //winning code block
+  if (playersGuess == winningNumber){
+    $("#guessLeft").text("You Win!");
+                      //add something in here to change you are hot etc to win
+    // $("#guessLeft").addClass("remainGuessWin");
+    $("#guessLeft").css({"font-family": "Aller_Bold",
+  "font-size": "80px", "letter-spacing": "2px","color": "red"})
+  }//end if
+  //incorrect guess code block
+  else{
+         var message = guessMessage(playersGuess);     //good spot to put in you are cold, hot etc
+        $("#youAre").text(message);
+        if(tries.indexOf(playersGuess) !== -1){
+          $("#youAre").text("Duplicate number,");
+          return;
+        }//end if
+
+    tries.push(playersGuess);
+    $("#tryAgainBlock").css("visibility","visible");
+    $("#tryAgainBlock").insertAfter( "#submitButton" );
+    var triesLeft=(5-(tries.length));
+      if (triesLeft == 0){
+        alert("Game Over, Man! You lose!");
+        $("#tryNum").text("0");
+      }
+      else{
+      $("#tryNum").text(triesLeft);
+        }
+  }//end else
+} //end checkGuess function
+
+// Create a provide hint button that provides additional clues to the "Player"
+
+function provideHint(){
+  function hintGenerator(){
+  var hintArray=[];
+  var hintCount=(((5-tries.length)*2)-1);
+  
+  function buildHints(){
+    var hintNum=Math.random();
+  hintNum*=100;
+  hintNum=Math.floor(hintNum);
+  return hintNum;
+  }//end buildHints
+ 
+  console.log("number of hints to give "+hintCount);
+  
+  hintArray.push(winningNumber);
+  for (i=0;i<hintCount;i++){
+    var num = buildHints();
+    if ((tries.indexOf(num)) == -1 && (hintArray.indexOf(num)) == -1  && num !==0){
+    hintArray.push(num);
+    }//end if
+    else{
+      i--;
+    }
+  }
+  hintArray.sort();
+  console.log(hintArray);
+  return hintArray;
+  
+}//end hintGenerator
+}// end provideHint
+
+// Allow the "Player" to Play Again
+
+function playAgain(){
+  // add code here
+}
+
+
+winningNumber=generateWinningNumber();
+
+/* **** Event Listeners/Handlers ****  */
+//get player's number guess with enter key
+$(document).on("keydown", function (event) {
+        if (event.which === 13){
+              playersGuessSubmission();
+        }//end if
+  });//end keydown function
+
+//get player's number guess with click key
+$( "#submitButtonInside" ).click(function() {
+  playersGuessSubmission();
+});
+
+
+
---
+++ styles.cs
@@ -0,0 +1,62 @@
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  /*background-image: url(clouds-20120607-IMG_0305-2.jpg) no-repeat center center fixed;*/
+  background: url("images/14170691_l_crop.jpg") no-repeat center center fixed;
+  background-size: cover;
+}
+
+@font-face {
+    font-family: anonymous;
+    src: url(fonts/Anonymous-webfont.ttf);
+}
+@font-face {
+    font-family: Aller_Bold;
+    src: url("fonts/Aller_Bd.ttf");
+}
+@font-face {
+    font-family: Aller_Reg;
+    src: url(fonts/Aller_Rg.ttf);
+}
+@font-face {
+    font-family: Aller_Letter;
+    src: url(fonts/Aller_Lt.ttf);
+}
+.stuff {
+  width: 400px;
+  height: 45%
+}
+
+#headline {
+  font-family: Aller_Bold;
+  font-size: 6em;
+  color: rgb(45,87,162);
+  font-style: bold;
+  margin: 0 auto;
+
+}
+#description{
+  font-family: Aller_Letter;
+  font-size: 1.5em;
+}
+
+.jumbotron{
+  width: 75%;
+  height: 75%;
+  background-color: rgba(191,223,251,0.7);
+  position: absolute;
+    top:0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+  text-align: center;
+  box-shadow: 25px 25px 20px rgba(12,67,139,0.8);
+  border-radius: 30px;
+}
+
+#submitButton{
+  margin: 0 auto;
+  text-align: center;
+}
