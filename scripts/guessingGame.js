e9a44e92541dd6024d29262566913f5ea2b799e4
John Hill <john@proofpositiveimaging.com>
2016-01-01 14:24:59 -0500

    Latest version of game, all functions working

--- scripts/guessingGame.js
+++ scripts/guessingGame.js
@@ -53,20 +53,24 @@ function guessMessage(playersGuess){
       return string;    
 } //end guessMessage
 
+
+
 // Check if the Player's Guess is the winning number 
 
 function checkGuess(playersGuess){
-  //winning code block
+
+  //correct guess & winning code block
   if (playersGuess == winningNumber){
     $("#guessLeft").text("You Win!");
-                      //add something in here to change you are hot etc to win
-    // $("#guessLeft").addClass("remainGuessWin");
     $("#guessLeft").css({"font-family": "Aller_Bold",
-  "font-size": "80px", "letter-spacing": "2px","color": "red"})
+  "font-size": "80px", "letter-spacing": "2px","color": "red"});
+    //animate win
+    animateWin();
   }//end if
+
   //incorrect guess code block
   else{
-         var message = guessMessage(playersGuess);     //good spot to put in you are cold, hot etc
+         var message = guessMessage(playersGuess); 
         $("#youAre").text(message);
         if(tries.indexOf(playersGuess) !== -1){
           $("#youAre").text("Duplicate number,");
@@ -78,14 +82,18 @@ function checkGuess(playersGuess){
     $("#tryAgainBlock").insertAfter( "#submitButton" );
     var triesLeft=(5-(tries.length));
       if (triesLeft == 0){
-        alert("Game Over, Man! You lose!");
-        $("#tryNum").text("0");
+        $("#guessLeft").text("Sorry, Game Over!");
+        $("#guessLeft").css({"font-family": "Aller_Bold",
+      "font-size": "60px", "letter-spacing": "2px","color": "red"});
+            $("#tryNum").text("0");
       }
       else{
       $("#tryNum").text(triesLeft);
         }
   }//end else
 } //end checkGuess function
+
+
 
 // Create a provide hint button that provides additional clues to the "Player"
 
@@ -113,9 +121,9 @@ function provideHint(){
     return hintArray;
     }//end hintGenerator
     //remove any li from page first
-  $("li").remove();
-  // var hintArray=hintGenerator();
-  hintGenerator();
+    $("li").remove();
+  // call the hint Generator
+      hintGenerator();
   
   //load hintArray into pop up hint page
   for (var i=0;i<hintArray.length; i++){
@@ -135,22 +143,65 @@ function provideHint(){
 
 }// end provideHint
 
+
 // Allow the "Player" to Play Again
 
 function playAgain(){
-location.reload();
+  winningNumber=0;
+  tries=[]
+  location.reload();
 }
+
+function animateWin(){
+        var div=$("#gameDiv");
+        div.animate({
+          height: '1000px',
+          width: '90%',
+          opacity: '0.5'
+        }, 
+          "slow");
+        div.animate({
+          height: '720px',
+          width: '75%',
+          opacity: '1.0'
+        }, 
+          "slow");
+
+          var text=$("#guessLeft");
+        text.animate({
+          letterSpacing: '30px'
+        }, 
+          "slow");
+        text.animate({
+          letterSpacing: '2px'
+        }, 
+          "slow");  
+
+         var text=$("#headline");
+        text.animate({
+          letterSpacing: '30px',
+          color: 'red'
+        }, 
+          "slow");
+        text.animate({
+          letterSpacing: '1px',
+          color: 'rgb(45,87,162)'
+        }, 
+          "slow"); 
+}
+
 
 
 winningNumber=generateWinningNumber();
 
 /* **** Event Listeners/Handlers ****  */
+
 //get player's number guess with enter key
 $(document).on("keydown", function (event) {
         if (event.which === 13){
               playersGuessSubmission();
         }//end if
-  });//end keydown function
+  });
 
 //get player's number guess with click key
 $( "#submitButtonInside" ).click(function() {
